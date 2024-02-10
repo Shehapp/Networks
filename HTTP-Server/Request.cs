eb0f51b00c8ba44845d80cb2a670103aa2bdd8c1
Shehapp <88383557+Shehapp@users.noreply.github.com>
2024-02-11 01:04:36 +0200

    impl of request

--- HTTP-Server/Request.cs
+++ HTTP-Server/Request.cs
@@ -38,29 +38,83 @@ namespace HTTPServer
         public Request(string requestString)
         {
             this.requestString = requestString;
+            this.headerLines = new Dictionary<string, string>();
         }
+
         /// <summary>
         /// Parses the request string and loads the request line, header lines and content, returns false if there is a parsing error
         /// </summary>
         /// <returns>True if parsing succeeds, false otherwise.</returns>
         public bool ParseRequest()
         {
-            throw new NotImplementedException();
 
             //TODO: parse the receivedRequest using the \r\n delimeter   
+            requestLines = requestString.Split(new string[] { "\r\n" }, StringSplitOptions.None);
 
             // check that there is atleast 3 lines: Request line, Host Header, Blank line (usually 4 lines with the last empty line for empty content)
+            if (requestLines.Length < 3)
+            {
+                return false;
+            }
 
             // Parse Request line
+            if (!ParseRequestLine())
+            {
+                return false;
+            }
 
             // Validate blank line exists
+            if (!ValidateBlankLine())
+            {
+                return false;
+            }
 
             // Load header lines into HeaderLines dictionary
+            if (!LoadHeaderLines())
+            {
+                return false;
+            }
+
+            return true;
         }
 
         private bool ParseRequestLine()
         {
-            throw new NotImplementedException();
+            string[] requestlineparts = requestLines[0].Split(' ');
+            if (requestlineparts.Length != 3)
+            {
+                return false;
+            }
+
+            // Validate Method
+            method = (RequestMethod)Enum.Parse(typeof(RequestMethod), requestlineparts[0]);
+            if (method == null)
+            {
+                return false;
+            }
+
+
+            // Validate URI
+            if (!ValidateIsURI(requestlineparts[1]))
+            {
+                return false;
+            }
+
+            relativeURI = requestlineparts[1];
+
+
+            // Validate HTTP Version
+            string httpversion = requestlineparts[2]
+                .Replace(".", "")
+                .Replace("/", "");
+
+            httpVersion = (HTTPVersion)Enum.Parse(typeof(HTTPVersion), httpversion);
+            if (httpVersion == null)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         private bool ValidateIsURI(string uri)
@@ -70,12 +124,34 @@ namespace HTTPServer
 
         private bool LoadHeaderLines()
         {
-            throw new NotImplementedException();
+            string[] ok;
+            for (int i = 1; requestLines[i] != ""; i++)
+            {
+                try
+                {
+                    ok = requestLines[i].Split(':');
+                    headerLines.Add(ok[0], ok[1]);
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
         }
 
         private bool ValidateBlankLine()
         {
-            throw new NotImplementedException();
+
+            if (requestLines[requestLines.Length - 2] != "")
+            {
+                return false;
+            }
+
+            return true;
+
         }
 
     }
